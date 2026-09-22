d8b48c77f694527a811c49c5d069b39afc7785b4
finn <158472281+finniiik@users.noreply.github.com>
2026-09-22 11:31:52 +0300

    ImageGallery

---
+++ src/ImageGallery.jsx
@@ -0,0 +1,11 @@
+function ImageGallery() {
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      <img src="https://picsum.photos/id/1015/200/150" alt="Река в горах" />
+      <img src="https://picsum.photos/id/1025/200/150" alt="Собака" />
+      <img src="https://picsum.photos/id/1035/200/150" alt="Лес" />
+    </div>
+  );
+}
+
+export default ImageGallery;
