27660de310e3c3f7efdd151c499148073c339bce
finn <158472281+finniiik@users.noreply.github.com>
2026-09-22 11:30:59 +0300

    TaskList

---
+++ src/TaskList.jsx
@@ -0,0 +1,12 @@
+function TaskList() {
+  return (
+    <ul>
+      <li>Позавтракать</li>
+      <li>Проверить почту</li>
+      <li>Сделать зарядку</li>
+      <li>Выпить стакан воды</li>
+    </ul>
+  );
+}
+
+export default TaskList;
