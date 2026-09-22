adbac3f812a794aee5524530283d44528c6258c7
finn <158472281+finniiik@users.noreply.github.com>
2026-09-22 11:32:07 +0300

    App

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Greeting from "./Greeting";
+import TaskList from "./TaskList";
+import ActionButton from "./ActionButton";
+import ProfileCard from "./ProfileCard";
+import ImageGallery from "./ImageGallery";
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+      <TaskList />
+      <ActionButton />
+      <ProfileCard />
+      <ImageGallery />
+    </div>
+  );
+}
+
+export default App;
